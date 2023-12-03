7d8042f56ac294d2e450f3a1aa2298dbb9eb7edc
unknown <Eklavya lalwani>
2023-12-04 01:06:31 +0530

    commiting changes for vercel

--- client/src/features/User/userAPI.js
+++ client/src/features/User/userAPI.js
@@ -1,13 +1,13 @@
 export  function fetchLoggedInUserOrders () {
   return new Promise (async (resolve) =>{
-  const response = await fetch('http://localhost:8080/orders/own')
+  const response = await fetch('https://react-ecommerce-website-three.vercel.app/orders/own')
   const data = await response.json()
   resolve({data})
   });
 }
 export  function fetchLoggedInUser () {
   return new Promise (async (resolve) =>{
-  const response = await fetch('http://localhost:8080/users/own' )
+  const response = await fetch('https://react-ecommerce-website-three.vercel.app/users/own' )
   const data = await response.json()
   resolve({data})
   });
@@ -15,7 +15,7 @@ export  function fetchLoggedInUser () {
 
 export function updateUser(update) {
   return new Promise(async (resolve) => {
-    const response = await fetch('http://localhost:8080/users/'+ update.id, {
+    const response = await fetch('https://react-ecommerce-website-three.vercel.app/users/'+ update.id, {
       method: 'PATCH',
       body: JSON.stringify(update),
       headers: { 'content-type': 'application/json' },
