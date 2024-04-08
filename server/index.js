54e78da2bd1874ff6d4e8cc0bc6089e472f7392d
unknown <Eklavya lalwani>
2024-04-08 20:18:42 +0530

    New build created

--- server/index.js
+++ server/index.js
@@ -144,38 +144,7 @@ server.post("/create-payment-intent", async (req, res) => {
   });
 });
 
-// Webhook
 
-// TODO: we will capture actual order after deploying out server live on public URL
-
-// const endpointSecret = "whsec_0e1456a83b60b01b3133d4dbe06afa98f384c2837645c364ee0d5382f6fa3ca2";
-
-// server.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
-//   const sig = request.headers['stripe-signature'];
-
-//   let event;
-
-//   try {
-//     event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-//   } catch (err) {
-//     response.status(400).send(`Webhook Error: ${err.message}`);
-//     return;
-//   }
-
-//   // Handle the event
-//   switch (event.type) {
-//     case 'payment_intent.succeeded':
-//       const paymentIntentSucceeded = event.data.object;
-//       console.log({paymentIntentSucceeded})
-//       // Then define and call a function to handle the event payment_intent.succeeded
-//       break;
-//     // ... handle other event types
-//     default:
-//       console.log(`Unhandled event type ${event.type}`);
-//   }
-//   // Return a 200 response to acknowledge receipt of the event
-//   response.send();
-// });
 
 main().catch(err=>console.log(err))
 async function main(){
